6eb3d3b4f32fe9f86b3f97e37686272c679545db
Pierre-Olivier Beaudoin <pierreolivier.beaudoin@gmail.com>
2016-12-15 12:26:54 -0500

    Sample 1 now takes an optionnal file to open as argument

--- samples/1/main.cpp
+++ samples/1/main.cpp
@@ -6,14 +6,25 @@
 
 using namespace std;
 
+string filenamePathFromArgs(int argc, char** argv)
+{
+    // Default value if no args
+    //string filenamePath = "../assets/sample/nested_references/master/master.flt";
+    //string filenamePath = "../assets/sample/nested_references2/db/1/1.flt";
+    string r = "../assets/sample/nested_references2/db/1/12/123/1234/1234.flt";
+    if(argc == 2)
+    {
+        r = argv[1];
+    }
+    return r;
+}
+
 int main(int argc, char** argv)
 {
     OpenFlight::OpenFlightReader ofr;
     ofr.enableDebug(true);
 
-    //string filenamePath = "../assets/sample/nested_references/master/master.flt";
-    //string filenamePath = "../assets/sample/nested_references2/db/1/1.flt";
-    string filenamePath = "../assets/sample/nested_references2/db/1/12/123/1234/1234.flt";
+    std::string filenamePath = filenamePathFromArgs(argc, argv);
 
     OpenFlight::HeaderRecord *pRoot = ofr.open( filenamePath );
     if ( !ofr.hasErrors() )
